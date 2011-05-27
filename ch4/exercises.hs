11bc7f72399ed8e45c1d8e320602b8e50cccb56d
Luke Amdor <luke.amdor@gmail.com>
2011-05-26 21:06:26 -0500

    exercises for asInt_*

--- ch4/exercises.hs
+++ ch4/exercises.hs
@@ -1,7 +1,8 @@
+import Data.Char (digitToInt, isDigit)
+    
 safeHead :: [a] -> Maybe a
 safeHead (x:xs) = Just x
 safeHead [] = Nothing
-
 
 safeTail :: [a] -> Maybe [a]
 safeTail [] = Nothing
@@ -12,19 +13,37 @@ safeLast [] = Nothing
 safeLast [x] = Just x
 safeLast (x:xs) = safeLast xs
 
-safeInit :: [a] -> Maybe [a]
+safeInit    :: [a] -> Maybe [a]
 safeInit [] = Nothing
 safeInit xs = Just $ init xs
 
-splitWith :: (a -> Bool) -> [a] -> [[a]]
-splitWith f xs = case break f xs of
-                   ([], []) -> []
-                   (xs2, []) -> [xs2]
-                   ([], ys2) -> splitWith f (tail ys2)
-                   (xs2, ys2) -> xs2 : splitWith f (tail ys2)
-                           
-splitWith2 :: (a -> Bool) -> [a] -> [[a]]
+splitWith       :: (a -> Bool) -> [a] -> [[a]]
+splitWith f xs  = case break f xs of
+                    ([], []) -> []
+                    (xs2, []) -> [xs2]
+                    ([], ys2) -> splitWith f (tail ys2)
+                    (xs2, ys2) -> xs2 : splitWith f (tail ys2)
+                            
+splitWith2      :: (a -> Bool) -> [a] -> [[a]]
 splitWith2 _ [] = []
 splitWith2 f xs = let (xs2, ys2) = break f $ dropWhile f xs
                   in xs2 : splitWith2 f (dropWhile f ys2)
-             
+
+asInt_fold :: String -> Int
+asInt_fold str = fst $ foldr step (0,0) str
+    where step '-' (acc, digit)         = (-acc, digit + 1)
+          step c (acc, digit)
+                            | isDigit c = (acc + (10 ^ digit) * digitToInt c,
+                                           digit + 1)
+                            | otherwise = error ("not a digit " ++ [c])
+
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either str = fst $ foldr step (Right 0, 0) str
+    where step '-' (Right acc, digit)   = (Right (-acc), digit + 1)
+          step c (Right acc, digit)
+                            | isDigit c = (Right (acc + (10 ^ digit) * digitToInt c),
+                                           digit + 1)
+                            | otherwise = (Left ("not a digit " ++ [c]), digit + 1)
+          step _ (l, digit)        = (l, digit + 1)
+          
